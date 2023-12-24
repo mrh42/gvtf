e9129188ea67fb56dcee7a581d36c6b1099d93ca
mrh <mrh@mrh.org>
2023-12-24 10:48:22 -0800

    big.Int version to handle K > 64-bits

--- main.go
+++ main.go
@@ -48,8 +48,8 @@ type Jtf struct {
 	Status     string   `json:"status"`
 	Bitlo      int64    `json:"bitlo"`
 	Bithi      int64    `json:"bithi"`
-	Begink     uint64   `json:"begink"`
-	Endk       uint64   `json:"endk"`
+	Begink     *big.Int   `json:"begink"`
+	Endk       *big.Int   `json:"endk"`
 	Rangec     bool     `json:"rangecomplete"`
 	Factors    []string `json:"factors,omitempty"`
 	Program    map[string]string  `json:"program,omitempty"`
@@ -84,13 +84,37 @@ func initInput(P uint64) {
 	C.mrhUnMap()
 }
 
-func tfRun(P, K1 uint64, bitlimit float64) {
+func u32n(N *big.Int, pos uint) uint32 {
+	m32 := big.NewInt(0xffffffff)
+
+	R := new(big.Int)
+	R.Rsh(N, pos * 32)
+	R.And(R, m32)
+	return uint32(R.Uint64())
+}
+func big3(u0, u1, u2 C.uint) *big.Int {
+	f := big.NewInt(int64(u2))
+	f.Lsh(f, 64)
+	f1 := big.NewInt(int64(u1))
+	f1.Lsh(f1, 32)
+	f.Or(f, f1)
+	f0 := big.NewInt(int64(u0))
+	f.Or(f, f0)
+	return f
+}
+
+func tfRun(P uint64, K1 *big.Int, bitlimit float64) {
 	p := (*C.struct_Stuff)(C.mrhGetMap());
 
-	K1 = C.M * (K1/C.M);
-	p.K[0] = C.uint(K1 & 0xffffffff)
-	p.K[1] = C.uint(K1>>32)
-	p.K[2] = 0
+	//K1 = C.M * (K1/C.M);
+	M := big.NewInt(C.M)
+	K:= new(big.Int)
+	K.Div(K1, M)
+	K.Mul(K, M)
+
+	p.K[0] = C.uint(u32n(K, 0))
+	p.K[1] = C.uint(u32n(K, 1))
+	p.K[2] = C.uint(u32n(K, 2))
 
 	p.L = 0
 	p.Init = 1
@@ -100,43 +124,49 @@ func tfRun(P, K1 uint64, bitlimit float64) {
 		p.Found[i][2] = 0
 	}
 
-	kfound := make([]uint64, 0, 10)
+	M2 := big.NewInt(C.M2)
+	kfound := make([]*big.Int, 0, 10)
 	mrhDone := false
-	k64 := K1
 	for {
 		p.Debug[0] = 0
 		p.Debug[1] = 0
-		p.KmodM2 = C.uint(k64 % C.M2)
+		KmM2 := new(big.Int)
+		KmM2.Mod(K, M2)
+		//p.KmodM2 = C.uint(k64 % C.M2)
+		p.KmodM2 = C.uint(KmM2.Uint64())
 		C.runCommandBuffer()
 
-		lb2 := math.Log2(float64(k64) * float64(P) * 2.0)
+		fk64, _ := K.Float64()
+		lb2 := math.Log2(fk64 * float64(P) * 2.0)
 		if lb2 > bitlimit {
 			mrhDone = true
 		}
-		for i := 0; i < 10; i++ {
-			f64 := uint64(p.Found[i][1])
-			f64 <<= 32
-			f64 |= uint64(p.Found[i][0])
-			if f64 != 0 {
-				kfound = append(kfound, f64)
-				flb2 := math.Log2(float64(f64) * float64(P) * 2.0)
-				fmt.Fprintf(os.Stderr, "# %d kfactor %d E: %d D: %d %.1f\n", P, f64, p.Debug[0], p.Debug[1], flb2);
+		if p.Debug[1] > 0 {
+			for i := 0; i < 10; i++ {
+				f := big3(p.Found[i][0], p.Found[i][1], p.Found[i][2])
+				f64, _ := f.Float64()
+				if f64 > 0 {
+					kfound = append(kfound, f)
+					flb2 := math.Log2(f64 * float64(P) * 2.0)
+					fmt.Fprintf(os.Stderr, "# %d kfactor %d E: %d D: %d %.1f\n", P, f, p.Debug[0], p.Debug[1], flb2);
 
-				p.Found[i][0] = 0;
-				p.Found[i][1] = 0;
-				//mrhDone = 1;
+					p.Found[i][0] = 0;
+					p.Found[i][1] = 0;
+					//mrhDone = 1;
+				}
 			}
 		}
+		
 		if (mrhDone) {
-			doLog(P, K1, k64, kfound)
+			doLog(P, K1, K, kfound)
 			break;
 		}
 		
 		if p.L >= p.Ll {
-			k64 += C.M
-			p.K[0] = C.uint(k64 & 0xffffffff)
-			p.K[1] = C.uint(k64>>32)
-			p.K[2] = 0
+			K.Add(K, M)
+			p.K[0] = C.uint(u32n(K, 0))
+			p.K[1] = C.uint(u32n(K, 1))
+			p.K[2] = C.uint(u32n(K, 2))
 
 			p.L = 0
 			p.Debug[0] = 0
@@ -147,11 +177,13 @@ func tfRun(P, K1 uint64, bitlimit float64) {
 	C.mrhUnMap();
 }
 
-func doLog(p, K1, K2 uint64, kfactors []uint64) {
-	bitlo := math.Log2(float64(K1) * float64(p) * 2.0 + 1)
-	bithi := math.Log2(float64(K2) * float64(p) * 2.0 + 1)
-	if K1 == 0 {
-		K1 = 1
+func doLog(p uint64, K1, K2 *big.Int, kfactors []*big.Int) {
+	k1f, _ := K1.Float64()
+	k2f, _ := K2.Float64()
+	bitlo := math.Log2(k1f * float64(p) * 2.0 + 1)
+	bithi := math.Log2(k2f * float64(p) * 2.0 + 1)
+	if k1f <= 1 {
+		K1.SetInt64(1)
 		bitlo = 1.0
 	}
 	stamp := time.Now().UTC().Format("2006-01-02 15:04:05")
@@ -162,8 +194,8 @@ func doLog(p, K1, K2 uint64, kfactors []uint64) {
 	One := big.NewInt(1)
 	factors := make([]*big.Int, 0, len(kfactors))
 	for _, k := range kfactors {
-		F := big.NewInt(int64(k))
-		F.Mul(F, P)
+		F := new(big.Int)
+		F.Mul(k, P)
 		F.Lsh(F, 1)
 		F.Add(F, One)
 		factors = append(factors, F)
@@ -191,13 +223,16 @@ func doLog(p, K1, K2 uint64, kfactors []uint64) {
 
 func main() {
 	//runtime.LockOSThread()
+	K1 := new(big.Int)
 
 	P := parseint(os.Args[1])
-	B := parsef(os.Args[2])
+	K1.SetString(os.Args[2], 10)
+	B2 := parsef(os.Args[3])
+
 	r := C.tfVulkanInit(C.sizeof_struct_Stuff, C.sizeof_struct_Stuff2)
 	if r == 0 {
 		initInput(P);
-		tfRun(P, 1, B);
+		tfRun(P, K1, B2);
 	}
 	C.cleanup()
 }
